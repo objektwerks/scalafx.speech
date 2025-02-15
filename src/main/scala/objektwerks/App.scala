5930ea9dfe4cc599c009435f39ca582a752e685c
objektwerks <objektwerks@runbox.com>
2025-02-15 11:00:55 -0500

    enhanced app

--- src/main/scala/objektwerks/App.scala
+++ src/main/scala/objektwerks/App.scala
@@ -1,12 +1,19 @@
 package objektwerks
 
+import com.typesafe.config.ConfigFactory
+//import com.typesafe.scalalogging.LazyLogging
+
 import scalafx.application.JFXApp3
 
 object App extends JFXApp3:
-  val context = Context()
+  val config = ConfigFactory.load("/app.conf")
+  val context = Context(config)
   val model = Model()
 
   override def start(): Unit =
     stage = new JFXApp3.PrimaryStage:
       scene = View(context, model).scene
       title = context.windowTitle
+      minWidth = context.windowWidth
+      minHeight = context.windowHeight
+      icons += context.imageAppIcon

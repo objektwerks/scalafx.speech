ead9da905856bbc58b8e5ea9b1c0e96f563b850e
objektwerks <objektwerks@runbox.com>
2025-02-15 11:58:24 -0500

    enhanced speech

--- src/main/scala/objektwerks/Speech.scala
+++ src/main/scala/objektwerks/Speech.scala
@@ -3,11 +3,20 @@ package objektwerks
 import com.google.cloud.texttospeech.v1.{AudioConfig, AudioEncoding, SynthesisInput, TextToSpeechClient, VoiceSelectionParams}
 
 final class Speech:
-  val voiceSelectionParams = VoiceSelectionParams.newBuilder().setLanguageCode("en-US").build()
-  val audioConfig = AudioConfig.newBuilder().setAudioEncoding(AudioEncoding.LINEAR16).build()
+  val voiceSelectionParams = VoiceSelectionParams
+    .newBuilder()
+    .setLanguageCode("en-US")
+    .build()
+  val audioConfig = AudioConfig
+    .newBuilder()
+    .setAudioEncoding(AudioEncoding.LINEAR16)
+    .build()
 
   def textToSpeech(text: String): Array[Byte] =
-    val synthesisInput = SynthesisInput.newBuilder().setText(text).build()
+    val synthesisInput = SynthesisInput
+      .newBuilder()
+      .setText(text)
+      .build()
     val textToSpeechClient = TextToSpeechClient.create()
     try
       textToSpeechClient.synthesizeSpeech(

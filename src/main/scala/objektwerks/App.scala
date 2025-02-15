b57433dff7c30bd9ad96ca470cad63d69f620955
objektwerks <objektwerks@runbox.com>
2025-02-15 11:09:45 -0500

    enhanced app

--- src/main/scala/objektwerks/App.scala
+++ src/main/scala/objektwerks/App.scala
@@ -3,13 +3,16 @@ package objektwerks
 import com.typesafe.config.ConfigFactory
 import com.typesafe.scalalogging.LazyLogging
 
+import java.awt.{Taskbar, Toolkit}
+import java.awt.Taskbar.Feature
+
 import scalafx.application.JFXApp3
+import scalafx.scene.image.Image
 
 object App extends JFXApp3 with LazyLogging:
   logger.info("Starting app ...")
 
-  val config = ConfigFactory.load("/app.conf")
-  val context = Context(config)
+  val context = Context(ConfigFactory.load("app.conf"))
   val model = Model()
 
   override def start(): Unit =
@@ -19,3 +22,17 @@ object App extends JFXApp3 with LazyLogging:
       minWidth = context.windowWidth
       minHeight = context.windowHeight
       icons += context.imageAppIcon
+
+    if Taskbar.isTaskbarSupported() then
+      val taskbar = Taskbar.getTaskbar()
+      if taskbar.isSupported(Feature.ICON_IMAGE) then
+        val defaultToolkit = Toolkit.getDefaultToolkit()
+        val appIcon = defaultToolkit.getImage(getClass().getResource("/image/icon.png"))
+        taskbar.setIconImage(appIcon)
+
+    stage.show()
+
+    logger.info("Started app.")
+
+  sys.addShutdownHook:
+    logger.info("Shutdown app.")

4d4752b061e5b6979328ebb59d58649ea6461ea8
objektwerks <objektwerks@runbox.com>
2025-02-15 15:48:50 -0500

    enhanced store

--- src/main/scala/objektwerks/Store.scala
+++ src/main/scala/objektwerks/Store.scala
@@ -9,32 +9,18 @@ import ox.*
 import scalafx.application.Platform
 
 final class Store extends LazyLogging:
-  os.makeDir.all( buildRecipesPath )
+  os.makeDir.all( buildFilesPath() )
 
-  private val recipesPath = buildRecipesPath
+  private val filesPath = buildFilesPath()
 
   logger.info("Initialized store.")
 
-  private def buildRecipesPath: Path = os.home / ".brewmeister" / "store" / "recipes"
+  private def buildFilesPath(): Path = os.home / ".speech" / "files"
 
   private def assertNotInFxThread: Unit = assert( !Platform.isFxApplicationThread, "Store operation called on Fx thread!" )
 
-  def listRecipes: List[Recipe] =
+  def writeFile(bytes: Array[Byte], file: String): Unit =
     supervised:
       assertNotInFxThread
-      os.list(recipesPath)
-        .filter { path => path.baseName.nonEmpty }
-        .map { path => readRecipe(s"${path.baseName}.json") }.toList
-
-  def writeRecipe(recipe: Recipe): Unit =
-    supervised:
-      assertNotInFxThread
-      val recipeAsJson = writeJson(recipe)
-      os.write.over(recipesPath / recipe.fileProperty.value, recipeAsJson)
-      logger.info(s"Write recipe: ${recipe.name}")
-
-  def removeRecipe(recipe: Recipe): Unit =
-    supervised:
-      assertNotInFxThread
-      os.remove(recipesPath / recipe.fileProperty.value)
-      logger.info(s"Remove recipe: ${recipe.name}")
+      os.write.over(filesPath / file, bytes)
+      logger.info(s"Write file: $file")

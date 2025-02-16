fa363a5ace4cb232455f74d7438678374047a07b
objektwerks <objektwerks@runbox.com>
2025-02-16 13:06:02 -0500

    enhanced media pane

--- src/main/scala/objektwerks/MediaPane.scala
+++ src/main/scala/objektwerks/MediaPane.scala
@@ -1,7 +1,10 @@
 package objektwerks
 
+import java.time.Instant
+
 import scalafx.scene.control.{Button, Label}
 import scalafx.scene.layout.HBox
+import scalafx.scene.media.{Media, MediaPlayer}
 
 final class MediaPane(context: Context, store: Store, speech: Speech) extends HBox:
   println(store)
@@ -12,7 +15,12 @@ final class MediaPane(context: Context, store: Store, speech: Speech) extends HB
     text = context.jokeButton
     onAction = { _ =>
       val joke = getJoke(context)
-      val bytes = speech.textToSpeech(joke)
+      speech.textToSpeech(joke) match
+        case Left(error) => textJoke.text = error.getMessage
+        case Right(bytes) => 
+          val uri = store.writeFile(bytes, s"${Instant.now.toString}.mp3")
+          val media = Media(uri)
+          MediaPlayer(media).play()
     }
 
   children = List(labelJoke, textJoke, buttonJoke)

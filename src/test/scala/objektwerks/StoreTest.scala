8a109cd5b2d25c325f1b152e8e12b590a2ba9ffe
objektwerks <objektwerks@runbox.com>
2025-02-16 10:30:58 -0500

    added store test

---
+++ src/test/scala/objektwerks/StoreTest.scala
@@ -0,0 +1,8 @@
+package objektwerks
+
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers
+
+class StoreTest extends AnyFunSuite with Matchers:
+  test("store"):
+    println("TODO")

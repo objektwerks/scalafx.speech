08c452bd3cf16e7b801983fc6cd492d654cf141b
objektwerks <objektwerks@runbox.com>
2025-02-16 07:21:27 -0500

    enhanced joke

--- src/main/scala/objektwerks/Joke.scala
+++ src/main/scala/objektwerks/Joke.scala
@@ -3,9 +3,9 @@ package objektwerks
 import scala.io.{Codec, Source}
 import scala.util.Using
 
-def getJoke(): String =
-  Using( Source.fromURL("https://api.chucknorris.io/jokes/random", Codec.UTF8.name) ) { 
+def getJoke(context: Context): String =
+  Using( Source.fromURL(context.jokeUrl, Codec.UTF8.name) ) { 
     source => s"* ${parseJson( source.mkString )}"
-  }.getOrElse("Chuck Norris is taking a power nap! Come back in a few nanoseconds!")
+  }.getOrElse(context.jokeError)
 
 private def parseJson(json: String): String = ujson.read(json)("value").str

75a712f3a5969005146dfb13660f9d5c3da6df1c
objektwerks <objektwerks@runbox.com>
2025-02-17 12:03:04 -0500

    edited joke

--- src/main/scala/objektwerks/Joke.scala
+++ src/main/scala/objektwerks/Joke.scala
@@ -5,7 +5,7 @@ import scala.util.Using
 
 def getJoke(context: Context): String =
   Using( Source.fromURL(context.jokeUrl, Codec.UTF8.name) ) { 
-    source => s"* ${parseJson( source.mkString )}"
+    source => parseJson( source.mkString )
   }.getOrElse(context.jokeError)
 
 private def parseJson(json: String): String = ujson.read(json)("value").str

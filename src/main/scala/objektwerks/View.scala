08d6057af1a27aaf08702d7a6586bdef4022b562
objektwerks <objektwerks@runbox.com>
2025-02-17 11:22:58 -0500

    enhanced view

--- src/main/scala/objektwerks/View.scala
+++ src/main/scala/objektwerks/View.scala
@@ -7,12 +7,14 @@ import scalafx.scene.layout.VBox
 final class View(context: Context,
                  store: Store,
                  speech: Speech):
+  val menu = Menu(context)
+
   val mediaPane = MediaPane(context, store, speech)
 
   val rootPane = new VBox:
     spacing = 6
     padding = Insets(6)
-    children = List(mediaPane)
+    children = List(menu, mediaPane)
 
   val scene = new Scene:
     root = rootPane
